09c51594701cc38444a8548ba2b1fbc0ac6f603b
sjackson4430 <sjackson4430@gmail.com>
2024-11-10 06:11:45 +0000

    fixed login

--- auth.js
+++ auth.js
@@ -145,5 +145,11 @@ class AuthManager {
         window.location.href = '/askit/login.html';
     }
 }
+404
+
+There isn't a GitHub Pages site here.
+
+If you're trying to publish one, read the full documentation to learn how to set up GitHub Pages for your repository, organization, or user account.
+GitHub Status — @githubstatus
 
 const auth = new AuthManager();
